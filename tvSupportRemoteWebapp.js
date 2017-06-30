025ed29020e6faf1edd17babde11a15daf1c988c
Stéphane Adam Garnier <seedsodesign@gmail.com>
2017-07-01 00:25:27 +0200

    Create tvSupportRemoteWebapp.js

---
+++ tvSupportRemoteWebapp.js
@@ -0,0 +1,11 @@
+/* tv support remote webapp V0.1a
+
+  StephaneAG - 2017
+*/
+console.log('tvSupportRemoteWebapp.js loaded');
+
+// TODO: add localStorage settings code
+
+// TODO: write app logic code
+
+// TODO: write 'holdToOrient' code
